42080c9d987adbe889d7ce74f0c8bded6e69f8ba
Victoria Gonzalez <vgonzalez3092@gmail.com>
2018-08-15 20:59:17 -0400

    js updates

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
 // Sets up the Express App
 var app = express();
@@ -11,15 +10,9 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var friendUsers = [];
 
-// Routes
-
-
-
-
-
-
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
