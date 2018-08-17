c850f5c0078b86e3de32c306ccb444cfdbc4ad9c
Victoria Gonzalez <vgonzalez3092@gmail.com>
2018-08-17 11:39:01 -0400

    updates on survey.html/friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,54 @@
 var friends = [
     {
-       
+        name: "Shakira",
+        picture: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Shakira_Stops_By_Soundcheck_cropped.jpg",
+        scores: [
+            "5",
+            "1",
+            "2",
+            "2",
+            "4",
+            "4",
+            "3",
+            "2",
+            "3",
+            "4"
 
+        ]
+    },
+    {
+        name: "Walt Disney",
+        picture: "https://upload.wikimedia.org/wikipedia/commons/d/df/Walt_Disney_1946.JPG",
+        scores: [
+            "1",
+            "3",
+            "5",
+            "2",
+            "3",
+            "4",
+            "1",
+            "1",
+            "3",
+            "5"
 
+        ]
+    },
+    {
+        name: "Bob",
+        picture: "https://vignette.wikia.nocookie.net/project-pokemon/images/4/47/Placeholder.png/revision/latest?cb=20170330235552",
+        scores: [
+            "3",
+            "2",
+            "2",
+            "2",
+            "5",
+            "5",
+            "2",
+            "2",
+            "2",
+            "1"
 
-    }
+        ]
+    },
 ]
 module.exports = friends
